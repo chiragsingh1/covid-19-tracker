3c636b7e93025c8f3077db4c8aa931810a45f095
chiragsingh1 <imchirag.2717@gmail.com>
2020-12-01 20:56:49 +0530

    creating infoboxes and dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { 
+  Card, 
+  CardContent, 
+  FormControl, 
+  MenuItem, 
+  Select 
+} from '@material-ui/core';
 import { useEffect, useState } from 'react';
+import InfoBox from './InfoBox';
 import './App.css';
+import Map from './Map';
 
 function App() {
   const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
@@ -24,19 +32,53 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    // console.log('COUNTRYCODE', countryCode);
+    setCountry(countryCode);
+  };
+
   return (
-    <div className="App">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+    <div className="app">
+
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={95566} total={3023334} />
+
+          <InfoBox title="Recovered" cases={3389} total={285509} />
+
+          <InfoBox title="Deaths" cases={23} total={68091} />
+        </div>
+        
+        {/* Map */}
+        <Map />
+
       </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <h3>Wordlwide New Cases</h3>
+          {/* Graph */}
+        </CardContent>
+                
+      </Card>
     </div>
   );
 }
