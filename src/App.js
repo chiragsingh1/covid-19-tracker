0797a29903b33143a6e1e95f0e74d21a7ca6019c
chiragsingh1 <imchirag.2717@gmail.com>
2020-12-01 23:42:12 +0530

    create table for cases by country and style it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { 
-  Card, 
-  CardContent, 
-  FormControl, 
-  MenuItem, 
-  Select 
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
 } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import InfoBox from './InfoBox';
-import './App.css';
 import Map from './Map';
+import Table from './Table';
+import './App.css';
+import { sortData } from './util';
 
 function App() {
-
   const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
@@ -29,7 +30,9 @@ function App() {
             name: country.country, // United Kingdom, INDIA
             value: country.countryInfo.iso2, // UK,IND
           }));
-          setTableData(data);
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -39,37 +42,33 @@ function App() {
   // useEffect to load worldwide cases when the app loads
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
-    .then(response => response.json())
-    .then(data => {
-      setCountryInfo(data);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
   }, []);
 
-
   const onCountryChange = async (event) => {
-
     const countryCode = event.target.value;
     // console.log('COUNTRYCODE', countryCode);
     setCountry(countryCode);
 
-    const url = 
-     countryCode === 'worldwide'
-     ? "https://disease.sh/v3/covid-19/all" 
-     : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
     await fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setCountry(countryCode);
-      setCountryInfo(data);
-    });
-    
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
   console.log('COUNTRY INFO>>>', countryInfo);
 
   return (
     <div className="app">
-
       <div className="app__left">
         <div className="app__header">
           <h1>COVID-19 TRACKER</h1>
@@ -88,40 +87,38 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox 
-            title="Coronavirus Cases" 
-            cases={countryInfo.todayCases} 
-            total={countryInfo.cases} 
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
           />
 
-          <InfoBox 
-            title="Recovered" 
-            cases={countryInfo.todayRecovered} 
-            total={countryInfo.recovered} 
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
           />
 
-          <InfoBox 
+          <InfoBox
             title="Deaths"
             cases={countryInfo.todayDeaths}
-            total={countryInfo.deaths} 
+            total={countryInfo.deaths}
           />
         </div>
-        
+
         {/* Map */}
         <Map />
-
       </div>
 
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
           {/* Table */}
+          <Table countries={tableData} />
           <h3>Wordlwide New Cases</h3>
           {/* Graph */}
         </CardContent>
-                
       </Card>
-       
     </div>
   );
 }
